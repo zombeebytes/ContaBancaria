3cac07f75a24b06323c00e61bbfe98d0427b2a00
unknown <andressapatricio93@gmail.com>
2023-12-19 08:26:44 -0300

    Exercicio Conta

---
+++ src/conta/Menu.java
@@ -0,0 +1,151 @@
+package conta;
+
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import conta.controller.ContaController;
+import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
+import conta.util.Cores;
+
+public class Menu {
+	public static Scanner leia = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		ContaController contas = new ContaController();
+
+		int opcao, numero, agencia, tipo, aniversario;
+		String titular;
+		float saldo, limite;
+
+		while (true) {
+
+			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND);
+
+			System.out.println("|*****************************************************|");
+			System.out.println("|                                                     |");
+			System.out.println("|                  BANCO DO GATIL                     |");
+			System.out.println("|                                                     |");
+			System.out.println("|*****************************************************|");
+			System.out.println("|                                                     |");
+			System.out.println("|            1 - Criar Conta                          |");
+			System.out.println("|            2 - Listar todas as Contas               |");
+			System.out.println("|            3 - Buscar Conta por Numero              |");
+			System.out.println("|            4 - Atualizar Dados da Conta             |");
+			System.out.println("|            5 - Apagar Conta                         |");
+			System.out.println("|            6 - Sacar                                |");
+			System.out.println("|            7 - Depositar                            |");
+			System.out.println("|            8 - Transferir valores entre Contas      |");
+			System.out.println("|            9 - Sair                                 |");
+			System.out.println("|                                                     |");
+			System.out.println("|_____________________________________________________|");
+			System.out.println(Cores.TEXT_GREEN_BOLD_BRIGHT +              "Entre com a opção desejada:"+Cores.TEXT_RESET);
+			System.out.println("                                                     ");
+
+			try {
+				opcao = leia.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("\nDigite Valores Inteiros: ");
+				leia.nextLine();
+				opcao = 0;
+			}
+			if (opcao == 9) {
+				System.out.println("\nBanco do Gatil - O seu mundo gateiro começa aqui!");
+				sobre();
+				leia.close();
+				System.exit(0);
+			}
+
+			switch (opcao) {
+			case 1:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Criar Conta\n\n");
+
+				System.out.println("Digite o Numero da Agência: \n");
+				agencia = leia.nextInt();
+				System.out.println("Digite o Nome do Titular: \n");
+				leia.skip("\\R?");
+				titular = leia.nextLine();
+
+				do {
+					System.out.println("Digite o Tipo da Conta: ");
+					System.out.println(Cores.TEXT_YELLOW +"(1 - Conta Corrente\n ou 2 - Conta Poupança): "+Cores.TEXT_RESET);
+					tipo = leia.nextInt();
+				} while (tipo < 1 && tipo > 2);
+
+				System.out.println("Digite o Saldo da Conta (R$): ");
+				saldo = leia.nextFloat();
+
+				switch (tipo) {
+				case 1 -> {
+					System.out.println("Digite o Limite de Crédito (R$): ");
+					limite = leia.nextFloat();
+					contas.cadastrar(new ContaCorrente(contas.gerarNumero(), agencia, tipo, titular, saldo, limite));
+				}
+				case 2 -> {
+					System.out.println("Digite o dia do Aniversário da Conta: ");
+					aniversario = leia.nextInt();
+					contas.cadastrar(
+							new ContaPoupanca(contas.gerarNumero(), agencia, tipo, titular, saldo, aniversario));
+
+				}
+				}
+
+				keyPress();
+				break;
+			case 2:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as Contas\n\n");
+				contas.listarTodas();
+				keyPress();
+				break;
+			case 3:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Consultar dados da Conta - por número\n\n");
+				keyPress();
+				break;
+			case 4:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar dados da Conta\n\n");
+				keyPress();
+				break;
+			case 5:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Apagar a Conta\n\n");
+				keyPress();
+				break;
+			case 6:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Saque\n\n");
+				keyPress();
+				break;
+			case 7:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Depósito\n\n");
+				keyPress();
+				break;
+			case 8:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Transferência entre Contas\n\n");
+
+				break;
+			default:
+				System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n");
+				keyPress();
+				break;
+			}
+		}
+	}
+
+	public static void sobre() {
+		System.out.println("\n*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+");
+		System.out.println("Projeto Desenvolvido por: Andressa Patricio Barbosa ");
+		System.out.println("Andressa Patricio Barbosa - andressapatricio93@gmail.com");
+		System.out.println("github.com/zombeebytes");
+		System.out.println("*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+");
+	}
+
+	public static void keyPress() {
+		try {
+			System.out.println(Cores.TEXT_RESET + "\n\nPressione Enter para Continuar");
+			System.in.read();
+
+		} catch (IOException e) {
+			System.out.println("Você pressionou uma tecla diferente de Enter!");
+		}
+	}
+}
---
+++ src/conta/model/ContaPoupanca.java
@@ -0,0 +1,24 @@
+package conta.model;
+import conta.model.Conta;
+
+public class ContaPoupanca extends Conta {
+	private int aniversario;
+	public ContaPoupanca(int numero, int agencia, int tipo, String titular, float saldo, int aniversario) {
+	
+		super(numero, agencia, tipo, titular, saldo);
+		this.aniversario = aniversario;
+		
+
+}
+	public int getAniversario() {
+		return aniversario;
+	}
+	public void setAniversario(int aniversario) {
+		this.aniversario = aniversario;
+	}
+	@Override 
+	public void visualizar() {
+		super.visualizar();
+		System.out.println("\nAniversário da Conta: " + this.aniversario);
+	}
+}
